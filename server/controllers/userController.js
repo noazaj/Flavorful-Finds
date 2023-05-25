5012fcc729af5e1adb3aa2c41c41acd2939c22b7
Noah Zajicek <zajicekn@flip1.engr.oregonstate.edu>
2023-05-25 11:08:11 -0700

    Add release function to users connection

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,8 +3,13 @@ const db = require('../../db-connector').pool
 
 // Read Users
 exports.read = (req, res) => {
+    
     // Execute a SQL query to fetch all user data.
     db.query('SELECT * FROM Users;', (err, results) => {
+        // Once the query is done and results returned, release 
+        // the database connection
+        db.release();
+
         if (err) {
             console.error('An error occurred while executing the query', err);
             res.status(500).send('An error occurred');
