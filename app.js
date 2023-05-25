3ece4015671b52b05ddfdc22257c3baa50daafb2
Noah Zajicek <zajicekn@flip1.engr.oregonstate.edu>
2023-05-24 20:25:01 -0700

    Made lots of changes

--- app.js
+++ app.js
@@ -1,86 +1,50 @@
-/*
-    SETUP
-*/
-// Express
-var express = require('express');   // We are using the express library for the web server
-var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 7070;                 // Set a port number at the top so it's easy to change in the future
+// SETUP
+const express   = require('express');       // We are using the express library for the web server
+const app       = express();                // We need to instantiate an express object to interact with the server in our code
+const PORT      = 7070;                     // Set a port number at the top so it's easy to change in the future
 
-/*
-    Handlebars
-*/
-// Require the express-handlebars module and 
-// set the view engine to Handlebars
-var exphbs = require('express-handlebars');
-const { query } = require('express');
-app.engine('.hbs', exphbs({
-    extname: '.hbs'
-}));
-app.set('view engine', 'handlebars');
+// HANDLEBARS
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+// const { query } = require('express');
+app.engine('hbs', exphbs.engine({extname: '.hbs'}));
+app.set('view engine', 'hbs');
 
-/*
-    Database
-*/
+// BODY PARSING MIDDLEWARE
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+// DATABASE
 // MySQL database connection
-var db = require('./db-connector.js')
+const db = require('./db-connector.js')
 
-/* 
-    ROUTES 
-*/
-app.get('/', function(req, res){
-    // Query for returning recipes
-    let query1 = 'SELECT title FROM Recipes;';
+// ROUTES
+app.get('/', (req, res) => {
+    // Render the index.hbs template
+    res.render('index');
+});
 
+
+app.get('/users', function(req, res){
+    // Query for returning Users
+    let query1 = 'SELECT * FROM Users;';
     db.pool.query(query1, function(err, rows, fields){
         // Send the results to the browser
-        res.send(rows);
+        if(err) {
+            // Handle the error
+            console.error(err);
+            res.status(500).send('Internal server error');
+        } else {
+            // Render handlebars template with data
+            res.render('users', { users: rows});
+        }
     });
 });
 
-/*
+// Use static files if not found in routes
+app.use(express.static('public'));
 
-    ROUTES
-
-app.get('/', function(req, res)
-    {
-        // Define our queries
-        query1 = 'DROP TABLE IF EXISTS diagnostic;';
-        query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
-        query3 = `INSERT INTO diagnostic (text) VALUES ("MySQL is working and I'm ready to start the project!!!!")`;
-        query4 = 'SELECT * FROM Recipes;';
-        query5 = 'SELECT * FROM diagnostic;';
-
-        // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
-
-        // DROP TABLE...
-        db.pool.query(query1, function (err, results, fields){
-
-            // CREATE TABLE...
-            db.pool.query(query2, function(err, results, fields){
-
-                // INSERT INTO...
-                db.pool.query(query3, function(err, results, fields){
-
-                    // SELECT *...
-                    db.pool.query(query4, function(err, results, fields){
-
-                        // SELECT *...
-                        db.pool.query(query5, function(err, results, fields){
-
-                            // Send the results to the browser
-                            let base = "<h1>MySQL Results:</h1>"
-                            res.send(base + JSON.stringify(results));
-                        });
-                    });
-                });
-            });
-        });
-    });
-*/
-
-/*
-    LISTENER
-*/
+// LISTENER
 app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
 });
