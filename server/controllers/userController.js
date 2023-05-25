215aef929c21c3ec57826dd90ac98ed5059bba6a
Noah Zajicek <zajicekn@flip1.engr.oregonstate.edu>
2023-05-25 11:17:30 -0700

    Add connection pool and release it once done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,21 +1,30 @@
 // MySQL database connection
-const db = require('../../db-connector').pool
+const db = require('../../db-connector').pool;
 
 // Read Users
 exports.read = (req, res) => {
     
-    // Execute a SQL query to fetch all user data.
-    db.query('SELECT * FROM Users;', (err, results) => {
-        // Once the query is done and results returned, release 
-        // the database connection
-        db.release();
-
+    // Get a connection from the connection pool.
+    db.getConnection((err, connection) => {
         if (err) {
-            console.error('An error occurred while executing the query', err);
+            console.error('An error occurred while getting the connection', err);
             res.status(500).send('An error occurred');
-        } else {
-            // Pass the fetched user data to the view.
-            res.render('users', { users: results });
+            return;
         }
+
+        // Execute a SQL query to fetch all user data.
+        connection.query('SELECT * FROM Users;', (err, results) => {
+            // Once the query is done and results returned, release 
+            // the database connection
+            connection.release();
+
+            if (err) {
+                console.error('An error occurred while executing the query', err);
+                res.status(500).send('An error occurred');
+            } else {
+                // Pass the fetched user data to the view.
+                res.render('users', { users: results });
+            }
+        });
     });
 };
